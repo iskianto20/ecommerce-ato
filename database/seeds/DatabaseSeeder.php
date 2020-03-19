8d42acab3d234c94911d6c5300ad91d978e5fffa
iskianto20 <abud032020@gmail.com>
2020-03-20 05:44:43 +0700

    Konfigurasi database, buat model dan migration, serta buat seeder beserta query buildernya

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\City;
+use App\Province;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
     }
 }
