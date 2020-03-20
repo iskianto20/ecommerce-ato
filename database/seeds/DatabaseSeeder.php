3c37c69383f1bcda9a639f512c6752e1ed8ba51b
iskianto20 <abud032020@gmail.com>
2020-03-20 16:14:56 +0700

    Templating dan authentication

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProvinceTableSeeder::class);
         $this->call(CityTableSeeder::class);
         $this->call(DistrictTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
